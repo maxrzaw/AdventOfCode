420a76e5c2d6dc0afccfe0b4af9a96605ff82811
Max Zawisa <maxrzaw@umich.edu>
2022-12-02 00:26:58 -0500

    clean up setup

--- setup.sh
+++ setup.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-day=`date +"%d"`
+# Get the day, I made it the second argument so I won't have to copy the cookie
+day=$2
 
 if [ -d $day ]
 then
-    echo Folder $day already exists
-    cd $day
-    return
+    echo Folder $day already exists. Not copying templates.
 else
     mkdir $day
+    cp template/* ${day}/
 fi
 
+echo Switching do directory "$day/"
 cd $day
 
-cp ../template/* ./
+# use the provided session cookie and request the input file
+trimmed_day=$(echo $day | sed 's/^0*//')
+curl -s -b "session=${1}" https://adventofcode.com/2022/day/${trimmed_day}/input -o input.txt
 
-echo "// You are all set up for day ${day}! Have fun!" >> a.ts
-vim a.ts
+echo You are all set up for day ${day}! Have fun!
