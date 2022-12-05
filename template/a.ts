962e0e3b49046fb151ebb674c498e944c64d2c3d
Max Zawisa <maxrzaw@umich.edu>
2022-12-05 01:39:29 -0500

    update template

--- template/a.ts
+++ template/a.ts
@@ -2,7 +2,12 @@ import * as fs from 'fs';
 export const x = "";
 
 const filename = process.argv[2];
-console.log(filename);
 
 const input = fs.readFileSync(filename, 'utf8');
+const sections = input.split('\n\n');
+const header = sections[0].trimEnd();
+const footer = sections[0].trimEnd();
+
+const lines = footer.split('\n').trimEnd();
+
 
