bcb9542b2d220f6c85c81c36c88179f84e4af482
Max Zawisa <maxrzaw@umich.edu>
2022-12-07 23:57:57 -0500

    change templates

--- template/b.ts
+++ template/b.ts
@@ -5,7 +5,3 @@ const filename = process.argv[2];
 
 const input = fs.readFileSync(filename, 'utf8');
 const sections = input.split('\n\n');
-const header = sections[0].trimEnd();
-const footer = sections[0].trimEnd();
-
-const lines = footer.split('\n');
