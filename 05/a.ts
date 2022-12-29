7c9721e2c87ec428069d28ab06d3cdf7f93f5118
Max Zawisa <maxrzaw@umich.edu>
2022-12-28 23:15:01 -0500

    clean up Day 5

--- 05/a.ts
+++ 05/a.ts
@@ -1,77 +1,28 @@
-import { log } from 'console';
-import * as fs from 'fs';
+import * as fs from "fs";
 export const x = "";
 
-class Stack {
-    // The stack is represented as an array
-    private items: any[] = [];
+const [header, moves] = fs.readFileSync(process.argv[2], "utf8").split("\n\n").map((s) => s.trimEnd().split("\n"));
+const stacksOne: string[][] = Array.from({ length: Math.ceil(header[0].length / 4) }, () => []);
+const stacksTwo: string[][] = Array.from({ length: Math.ceil(header[0].length / 4) }, () => []);
 
-    // The push() method adds an element to the top of the stack
-    push(item: any) {
-        this.items.push(item);
-    }
-
-    pushBack(item: any) {
-        this.items.unshift(item);
-    }
-
-    // The pop() method removes and returns the top element of the stack
-    pop() {
-        return this.items.pop();
-    }
-
-    // The peek() method returns the top element of the stack without removing it
-    peek() {
-        return this.items[this.items.length - 1];
-    }
-
-    // The isEmpty() method returns true if the stack is empty, and false otherwise
-    isEmpty() {
-        return this.items.length === 0;
-    }
-}
-
-const filename = process.argv[2];
-
-const input = fs.readFileSync(filename, 'utf8');
-
-const setup = input.split('\n\n');
-const stackCount = Math.ceil(setup[0].split('\n')[0].length / 4);
-const stacks: Stack[] = [];
-
-for (let i = 0; i < stackCount; ++i) {
-    const stack = new Stack();
-    stacks.push(stack);
-}
-
-let starting = setup[0].trimEnd().split('\n');
-
-for (let line of starting) {
-    const crates: string[] = new Array<string>().fill("", stackCount);
+for (let line of header.slice(0, -1)) {
+    const crates: string[] = new Array<string>().fill("", Math.ceil(header[0].length / 4));
     let j = 0;
     for (let i = 0; i < line.length; i += 4) {
-        const crate = line.slice(i, i + 4)[1];
         crates[j] = line.slice(i, i + 4)[1];
-        if (crate !== " ") {
-            stacks[j].pushBack(crate);
+        if (crates[j] !== " ") {
+            stacksOne[j].unshift(crates[j]);
+            stacksTwo[j].unshift(crates[j]);
         }
         ++j;
     }
 }
 
-const instructions = setup[1].trimEnd().split('\n');
-
-for (let instruction of instructions) {
-    const line = instruction.split(' ');
-
-    const count = parseInt(line[1]);
-
-    const start = parseInt(line[3]) - 1;
-    const end = parseInt(line[5]) - 1;
-
-    for (let i = 0; i < count; ++i) {
-        stacks[end].push(stacks[start].pop());
-    }
+for (let move of moves) {
+    const [count, start, end] = move.split(" ").map((s) => parseInt(s)).filter((s) => !isNaN(s));
+    stacksOne[end - 1].push(...stacksOne[start - 1].splice(-1 * count).reverse());
+    stacksTwo[end - 1].push(...stacksTwo[start - 1].splice(-1 * count));
 }
 
-log(stacks.map((s) => s.peek()).join(''));
+console.log(`Part One: ${stacksOne.map((s) => s.at(s.length - 1)).join("")}`);
+console.log(`Part Two: ${stacksTwo.map((s) => s.at(s.length - 1)).join("")}`);
