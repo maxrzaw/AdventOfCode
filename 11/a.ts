73bf5ae0a4d3d3f837042420968a76655fb16633
Max Zawisa <maxrzaw@umich.edu>
2022-12-29 00:50:57 -0500

    clean up day 11

--- 11/a.ts
+++ 11/a.ts
@@ -1,5 +1,7 @@
-import { log } from 'console';
 export const x = "";
+const PART = parseInt(process.argv[2]);
+const ROUNDS = parseInt(process.argv[3]);
+let mod: number = 1;
 
 class Monkey {
     private items: number[];
@@ -17,20 +19,28 @@ class Monkey {
         this._test = test;
     }
 
+    public GetTestMod(): number {
+        return this._test;
+    }
+
     private test(item: number): boolean {
         return item % this._test === 0;
     }
 
     public TakeTurn(monkeys: Monkey[]): void {
         while (this.items.length > 0) {
-            let item = this.items.shift() as number;
-            item = this._inspect(item);
-            item = Math.floor(item / 3);
+            let item = this.items.shift();
+            if (PART === 1) {
+                item = Math.floor(this._inspect(item) / 3);
+            } else {
+                item = this._inspect(item) % mod;
+            }
             const destination = this.test(item) ? this.trueTarget : this.falseTarget;
             monkeys[destination].AddItem(item);
             this.inspectionsPerformed++;
         }
     }
+
     public GetInspections(): number {
         return this.inspectionsPerformed;
     }
@@ -40,9 +50,8 @@ class Monkey {
     }
 }
 
-const monkeys: Monkey[] = [];
-
 // I used a vim macro to create these from the inputs ;)
+const monkeys: Monkey[] = [];
 monkeys.push(new Monkey([91, 58, 52, 69, 95, 54], 1, 5, (old: number) => old * 13, 7));
 monkeys.push(new Monkey([80, 80, 97, 84], 3, 5, (old: number) => old * old, 3));
 monkeys.push(new Monkey([86, 92, 71], 0, 4, (old: number) => old + 7, 2));
@@ -52,37 +61,20 @@ monkeys.push(new Monkey([96, 52, 52, 94, 76, 51, 57], 7, 3, (old: number) => old
 monkeys.push(new Monkey([75], 4, 2, (old: number) => old + 5, 13));
 monkeys.push(new Monkey([83, 75], 2, 6, (old: number) => old + 1, 19));
 
-const exampleMonkeys: Monkey[] = [];
-exampleMonkeys.push(new Monkey([79, 98], 2, 3, (old: number) => old * 19, 23));
-exampleMonkeys.push(new Monkey([54, 65, 75, 74], 2, 0, (old: number) => old + 6, 19));
-exampleMonkeys.push(new Monkey([79, 60, 97], 1, 3, (old: number) => old * old, 13));
-exampleMonkeys.push(new Monkey([74], 0, 1, (old: number) => old + 3, 17));
-
-for (let i = 0; i < 20; ++i) {
-    for (let monkey of exampleMonkeys) {
-        monkey.TakeTurn(exampleMonkeys);
-    }
+for (let monkey of monkeys) {
+    mod *= monkey.GetTestMod();
 }
 
-for (let i = 0; i < 20; ++i) {
+for (let i = 0; i < ROUNDS; ++i) {
     for (let monkey of monkeys) {
         monkey.TakeTurn(monkeys);
     }
 }
 
-log("Example:");
-let exampleInspections: number[] = [];
-for (let monkey of exampleMonkeys) {
-    exampleInspections.push(monkey.GetInspections());
-}
-exampleInspections.sort((a, b) => b - a);
-log(exampleInspections, '\n', exampleInspections[0] * exampleInspections[1]);
-
-log("\nMy Input:");
 let inspections: number[] = [];
 for (let monkey of monkeys) {
     inspections.push(monkey.GetInspections());
 }
 
 inspections.sort((a, b) => b - a);
-log(inspections, '\n', inspections[0] * inspections[1]);
+console.log(inspections[0] * inspections[1]);
