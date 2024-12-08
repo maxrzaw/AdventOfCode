36bf890e83d1de4f66b843fe0e64b77243c1f2be
Max Zawisa <maxrzaw@umich.edu>
2024-12-08 10:46:50 -0500

    update setup.sh for 2024

--- 2024/setup.sh
+++ 2024/setup.sh
@@ -3,19 +3,19 @@
 # Get the day, I made it the second argument so I won't have to copy the cookie
 day=$2
 
-if [ -d $day ]
+if [ -d day$day ]
 then
-    echo Folder $day already exists. Not copying templates.
+    echo Folder day$day already exists. Not copying templates.
 else
-    mkdir $day
-    cp template/* ${day}/
+    mkdir day$day
+    cp template/* day${day}/
 fi
 
-echo Switching do directory "$day/"
-cd $day
+echo Switching do directory "day${day}/"
+cd day$day
 
 # use the provided session cookie and request the input file
 trimmed_day=$(echo $day | sed 's/^0*//')
-curl -s -b "session=${1}" https://adventofcode.com/2022/day/${trimmed_day}/input -o input.txt
+curl -s -b "session=${1}" https://adventofcode.com/2024/day/${trimmed_day}/input -o input.txt
 
 echo You are all set up for day ${day}! Have fun!
