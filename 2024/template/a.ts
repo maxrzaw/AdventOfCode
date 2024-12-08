b8c896e214d1c1c13aec9ece05c5e1643f16c598
Max Zawisa <maxrzaw@umich.edu>
2024-12-08 13:34:41 -0500

    update template

--- 2024/template/a.ts
+++ 2024/template/a.ts
@@ -1,9 +1,8 @@
 import * as fs from 'fs';
-export const ex = "";
+export const ex = '';
 
 const filename = process.argv[2];
 
 const input = fs.readFileSync(filename, 'utf8');
-const sections = input.split('\n\n');
+const sections = input.split('\n').filter((x) => x !== '');
 const lines = input.split('\n');
-
